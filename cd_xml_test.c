bdafeabf037a2d05a9002299046a2f478fe51d39
Christopher Dyken <cdyk@users.noreply.github.com>
2019-12-26 23:02:06 +0100

    Namespace example

--- cd_xml_test.c
+++ cd_xml_test.c
@@ -11,13 +11,28 @@
 
 int main(int argc, const char * argv[]) {
     
-    {
+    {   // Misc UTF-tests
         const char* xml =
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!--m oo-->"
             "<foo moo=' doo  '><gah quux='waldo&lt;&#x5d0;'>   ᚠᚢᚦ&amp;ᚨᚱᚲ €  <meep/> æøå 𠜎  </gah><meh>meh</meh></foo>";
         cd_xml_doc_t doc;
-        cd_xml_rv_t rv = cd_xml_parse(&doc, xml, strlen(xml));        assert(rv == CD_XML_SUCCESS);
-        
+        cd_xml_rv_t rv = cd_xml_parse(&doc, xml, strlen(xml));
+        assert(rv == CD_XML_SUCCESS);
     }
+    {   // Namespaces
+        const char* xml =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            "<foo xmlns='http://a.com'>\n"
+            "  <bar xmlns:b='http://b.com'>\n"
+            "    <baz xmlns:c='http://c.com'>\n"
+            "     <b:quux/>\n"
+            "    </baz>\n"
+            "  </bar>\n"
+            "</foo>\n";
+        cd_xml_doc_t doc;
+        cd_xml_rv_t rv = cd_xml_parse(&doc, xml, strlen(xml));
+        assert(rv == CD_XML_SUCCESS);
+    }
+
     return 0;
 }
